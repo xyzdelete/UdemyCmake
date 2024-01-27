e799051d54e94c1fcd59125eb94795e549a5d6b6
Arturs <xyzdelete@protonmail.com>
2024-01-27 13:00:07 +0200

    21. External Libraries: Git Submodule

--- 2_CMake/HelloWorld/app/main.cpp
+++ 2_CMake/HelloWorld/app/main.cpp
@@ -1,12 +1,18 @@
 #include <iostream>
 
-#include "my_lib.h"
-#include "config.hpp"
+#include <my_lib.h>
+#include <config.hpp>
+#include <nlohmann/json.hpp>
 
 int main()
 {
     std::cout << project_name << std::endl;
     std::cout << project_version << std::endl;
+
+    std::cout << "JSON Lib Version: "
+        << NLOHMANN_JSON_VERSION_MAJOR << "."
+        << NLOHMANN_JSON_VERSION_MINOR << "."
+        << NLOHMANN_JSON_VERSION_PATCH << std::endl;
 
     print_hello_world();
     return 0;
