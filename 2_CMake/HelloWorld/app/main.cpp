839c6a635d7f35c1ed48badeb61d6a3c895184c3
Arturs <xyzdelete@protonmail.com>
2024-01-28 11:15:37 +0200

    23. External Libraries: Fetch Content

--- 2_CMake/HelloWorld/app/main.cpp
+++ 2_CMake/HelloWorld/app/main.cpp
@@ -3,6 +3,9 @@
 #include <my_lib.h>
 #include <config.hpp>
 #include <nlohmann/json.hpp>
+#include <cxxopts.hpp>
+#include <fmt/format.h>
+#include <spdlog/spdlog.h>
 
 int main()
 {
@@ -10,9 +13,31 @@ int main()
     std::cout << project_version << std::endl;
 
     std::cout << "JSON Lib Version: "
-        << NLOHMANN_JSON_VERSION_MAJOR << "."
-        << NLOHMANN_JSON_VERSION_MINOR << "."
-        << NLOHMANN_JSON_VERSION_PATCH << std::endl;
+        << NLOHMANN_JSON_VERSION_MAJOR
+        << "."
+        << NLOHMANN_JSON_VERSION_MINOR
+        << "."
+        << NLOHMANN_JSON_VERSION_PATCH
+        << std::endl;
+
+    std::cout << "FMT Lib Version: "
+        << FMT_VERSION << std::endl;
+
+    std::cout << "CXXOPTS Lib Version: "
+        << CXXOPTS__VERSION_MAJOR
+        << "."
+        << CXXOPTS__VERSION_MINOR
+        << "."
+        << CXXOPTS__VERSION_PATCH
+        << std::endl;
+
+    std::cout << "SPDLOG Lib Version: "
+        << SPDLOG_VER_MAJOR
+        << "."
+        << SPDLOG_VER_MINOR
+        << "."
+        << SPDLOG_VER_PATCH
+        << std::endl;
 
     print_hello_world();
     return 0;
--- 2_CMake/HelloWorld/src/my_lib/my_lib.cpp
+++ 2_CMake/HelloWorld/src/my_lib/my_lib.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
+#include <my_lib.h>
 
-#include "my_lib.h"
+#include <nlohmann/json.hpp>
 
 void print_hello_world()
 {
-    std::cout << "Hello World!\n";
+    std::cout << "Hello World!" << std::endl;
+
+    std::cout << "JSON Lib Version: "
+        << NLOHMANN_JSON_VERSION_MAJOR << "."
+        << NLOHMANN_JSON_VERSION_MINOR << "."
+        << NLOHMANN_JSON_VERSION_PATCH << std::endl;
 }
